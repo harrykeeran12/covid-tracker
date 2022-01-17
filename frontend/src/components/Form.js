70bc4469c4cd5f8ec84f5c9f2ea369bcc69566d9
Harry <68599167+harrykeeran12@users.noreply.github.com>
2022-01-17 17:06:47 +0000

    made react-select work
    fixed react-select
    dropdown box works properly, and can select the value.

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -2,19 +2,23 @@ import './Form.css';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Select from 'react-select';
-import makeAnimated from 'react-select/animated';
 function Form() {
   const [FormList, setFormList] = useState([])
+  const [Selected, setSelected] = useState('')
   const api = axios.create({baseURL: 'http://localhost:3001'})
-  let options = []
+  let options = [];
+
+  const handleChange = e => {
+    setSelected(e.value);
+  }
   useEffect(() => {
     api.get('/countries').then(res=>{
       let temp = res.data
-      temp.length = 225;
+      temp.length = 224;
       temp.sort();
       
       setFormList(temp)
-      console.log(FormList)
+      /* console.log(FormList) */
     })
   }, [])
 
@@ -22,10 +26,18 @@ function Form() {
   temp.forEach(e => {
     options.push({value: e, label: e})
   });
-  console.log(options)
+  console.log(Selected)
+  /* console.log(options) */
   return ( 
     <div className='FormWrapper'>
-    <Select options={options} className='SelectBox' placeholder='Select a country to find the data of:'isSearchable></Select>
+    <Select options={options} 
+    className='SelectBox' 
+    placeholder='Select a country to find the data of:'
+    isSearchable 
+    makeAnimated
+    value={options.find(obj => obj.value === Selected)}
+    onChange={handleChange}
+    ></Select>
     </div>
    );
 }
